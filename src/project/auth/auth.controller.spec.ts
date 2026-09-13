3e51c08b4f362cd5bd7f53065e9c803504af04af
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2026-09-13 19:21:31 +0900

    test: Jest ESM 및 테스트 의존성 설정 수정 (#14)

--- src/project/auth/auth.controller.spec.ts
+++ src/project/auth/auth.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { User } from '../users/entities/user.entity';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +12,17 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: getRepositoryToken(User), useValue: {} },
+        { provide: JwtService, useValue: {} },
+        {
+          provide: ConfigService,
+          useValue: {
+            getOrThrow: jest.fn().mockReturnValue('http://localhost:5173'),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
